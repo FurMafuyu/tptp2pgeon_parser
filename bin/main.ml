0789848d5640a92ce63c7e26932e3aabd8fb3de3
FurMafuyu <matthieutvrg@gmail.com>
2026-05-22 10:43:39 +0200

    Added negation of Theorem files

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,7 @@
 open Tptp_parser
+open Ast
+
+let debug = false
 
 let print_error_position lexbuf =
   let pos = lexbuf.Lexing.lex_curr_p in
@@ -12,29 +15,52 @@ let () =
     Printf.printf "Usage: %s <file.p>\n" Sys.argv.(0)
   else
     let filename = Sys.argv.(1) in
+    let status = Status.extract_status filename in
+    
+    let should_negate = 
+      match status with
+      | Status.Unsatisfiable -> 
+          if debug then Printf.eprintf "[DEBUG] Detected Unsatisfiable status. Keeping formulas as they are.\n";
+          false
+      | Status.Theorem -> 
+          if debug then Printf.eprintf "[DEBUG] Detected Theorem status (S4 Constant if modal). Enabling negation.\n";
+          true
+      | Status.NonTheorem ->
+          if debug then Printf.eprintf "[DEBUG] Formula is Non-Theorem, keeping formula as they are, should succeed.\n";
+          false
+      | Status.Satisfiable ->
+          if debug then Printf.eprintf "[DEBUG] Satisfiable formula, keeping formula as they are, should fail/timeout.\n";
+          false
+      | Status.Unsolved | Status.UnknownStatus ->
+          if debug then Printf.eprintf "[DEBUG] Error: Status is Unsolved / Unknown. Aborting.\n";
+          exit 1
+    in
+
     let ic = open_in filename in
     let lexbuf = Lexing.from_channel ic in
-    
     lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filename };
 
     try
       let ast = Parser.problem Lexer.token lexbuf in
-      Writer.print_problem ast;
+      
+      let final_ast = 
+        if should_negate then
+          let closed_formulas = List.map Writer.close_formula ast.formulas in
+          match closed_formulas with
+          | [] -> ast
+          | [seule_formule] -> { formulas = [EApp ("not", [seule_formule])] }
+          | premiere :: reste ->
+              let grosse_conjonction = 
+                List.fold_left (fun acc_expr f -> EApp ("and", [acc_expr; f])) premiere reste
+              in
+              { formulas = [EApp ("not", [grosse_conjonction])] }
+        else 
+          ast
+      in
+
+      Writer.print_problem final_ast;
       close_in ic
     with
-    | Parser.Error ->
-        close_in ic;
-        print_error_position lexbuf;
-        Printf.eprintf "Parser error: The structural framework of the formula is invalid here.\n";
-        exit 1
-
-    | Lexer.Lexing_error msg ->
-        close_in ic;
-        print_error_position lexbuf;
-        Printf.eprintf "Lexical error: %s\n" msg;
-        exit 1
-
-    | e ->
-        close_in ic;
-        Printf.eprintf "Unexpected error: %s\n" (Printexc.to_string e);
-        exit 1
+    | Parser.Error -> close_in ic; print_error_position lexbuf; exit 1
+    | Lexer.Lexing_error msg -> close_in ic; print_error_position lexbuf; exit 1
+    | e -> close_in ic; Printf.eprintf "Unexpected error: %s\n" (Printexc.to_string e); exit 1
---
+++ lib/status.ml
@@ -0,0 +1,73 @@
+type tptp_status = 
+  | Theorem 
+  | Unsatisfiable 
+  | NonTheorem 
+  | Satisfiable 
+  | Unsolved 
+  | UnknownStatus
+
+let contains sub s =
+  try
+    let _ = Str.search_forward (Str.regexp_string sub) s 0 in
+    true
+  with Not_found -> false
+
+let string_to_status s =
+  let s = String.lowercase_ascii s in
+  if contains "unsatisfiable" s then Unsatisfiable
+  else if contains "non-theorem" s then NonTheorem
+  else if contains "theorem" s then Theorem
+  else if contains "satisfiable" s then Satisfiable
+  else if contains "unsolved" s then Unsolved
+  else UnknownStatus
+
+let extract_status filename =
+  let ic = open_in filename in
+  
+  let rec parse_lines is_qmltp current_status =
+    try
+      let line = input_line ic in
+      if String.length line > 0 && line.[0] <> '%' then current_status
+      else
+        let l_low = String.lowercase_ascii line in
+        
+        if contains "file" l_low && contains "qmltp" l_low then
+          parse_lines true current_status
+          
+        else if is_qmltp && contains "s4" l_low then
+          (* NOUVEAU : Si on a DÉJÀ trouvé un statut valide au tour d'avant, 
+             on ignore les lignes S4 suivantes (comme celle du Rating) *)
+          if current_status <> UnknownStatus then 
+            parse_lines is_qmltp current_status
+          else
+            let clean_line = String.map (fun c -> if c = '\t' then ' ' else c) l_low in
+            let words = String.split_on_char ' ' clean_line 
+                        |> List.filter (fun s -> s <> "") in
+            
+            match words with
+            | "%" :: "s4" :: _varying :: _cumulative :: constant_status :: _ ->
+                let status = string_to_status constant_status in
+                parse_lines is_qmltp status
+            | _ -> 
+                let rec find_constant_before_version = function
+                  | status_candidate :: version_str :: _ when String.length version_str > 0 && version_str.[0] = 'v' ->
+                      string_to_status status_candidate
+                  | _ :: rest -> find_constant_before_version rest
+                  | [] -> current_status
+                in
+                let status = find_constant_before_version words in
+                parse_lines is_qmltp status
+
+        else if not is_qmltp && contains "status" l_low then
+          let status = string_to_status l_low in
+          parse_lines is_qmltp status
+          
+        else
+          parse_lines is_qmltp current_status
+    with End_of_file ->
+      current_status
+  in
+  
+  let final_status = parse_lines false UnknownStatus in
+  close_in_noerr ic;
+  final_status
